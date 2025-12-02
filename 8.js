56aab7fe44486452c30c023d021499f47235a1d2
Sahil Chouhan <chouhangolu604@gmail.com>
2025-12-02 16:28:43 +0530

    new file

---
+++ 8.js
@@ -0,0 +1,16 @@
+function numberPalindrome(num){
+    num = num+"";
+    let reverseNum = ''
+    for(let i=num.length-1; i>=0; i--){
+        reverseNum += num[i]
+    }
+    if(reverseNum===num){
+        return true
+    } else{
+        return false
+    }
+}
+console.log(numberPalindrome(1221))
+console.log(numberPalindrome(121))
+console.log(numberPalindrome(122221))
+console.log(numberPalindrome(125))
